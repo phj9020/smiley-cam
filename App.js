b559ca71f2bcae0628513b0aeae5ffeffdd02407
HanJin Park <phj9020@hotmail.com>
2021-03-22 16:22:10 +0900

    smile gauge added

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
-import React, { useEffect, useState } from "react";
-import { ActivityIndicator, Dimensions, TouchableOpacity } from "react-native";
+import React, { useEffect, useState, useRef } from "react";
+import { ActivityIndicator, Dimensions, TouchableOpacity, View } from "react-native";
 import { Camera } from "expo-camera";
 import styled from "styled-components/native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
+import * as Permissions from 'expo-permissions';
+import * as FaceDetector from "expo-face-detector";
+import * as MediaLibrary from 'expo-media-library';
+import { MEDIA_LIBRARY } from "expo-permissions";
 
 const { width: WIDTH, height: HEIGHT } = Dimensions.get("window");
 
@@ -21,6 +25,9 @@ const Text = styled.Text`
 export default function App() {
   const [hasPermission, setHasPermission] = useState(null);
   const [cameraType, setCameraType] = useState(Camera.Constants.Type.front);
+  const [smileDetected, setSmileDetected] = useState(false);
+  const [smilePercentage, setSmilePercentage] = useState(0)
+  const cameraRef = useRef();
 
   useEffect(() => {
     (async () => {
@@ -40,8 +47,64 @@ export default function App() {
       setCameraType(Camera.Constants.Type.front);
     }
   };
-  console.log(hasPermission);
+  
+  const onFacesDetected = ({faces}) => {
+    const face = faces[0];
+    if(face) {
+      setSmilePercentage(face.smilingProbability);
+      if(face.smilingProbability > 0.7) {
+        setSmileDetected(true)
+        // take pick
+        takePhoto();
+      }
+    }
+  }
 
+  const takePhoto = async () => {
+    try{
+      if(cameraRef.current) {
+        // photo.uri 
+        let {uri} = await cameraRef.current.takePictureAsync({quality: 1});
+
+        if(uri) {
+          savePhoto(uri);
+        }
+      }
+    } catch (error) {
+      alert(error);
+      setSmileDetected(false)
+    }
+  }
+
+  // move photo in temporary to local phone
+  const savePhoto = async (uri) => {
+    try{
+        //permissions ask
+        const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+        if(status === "granted") {
+          // take photo 
+          const asset = await MediaLibrary.createAssetAsync(uri);
+          // find album
+          let album = await MediaLibrary.getAlbumAsync('Smiley-Cam');
+          
+          // album 이 없으면 만들자
+          if(album === null) {
+            album = await MediaLibrary.createAlbumAsync('Smiley-Cam', asset);
+          } else {
+            await MediaLibrary.addAssetsToAlbumAsync([asset], album.id);
+          }
+
+          setSmileDetected(false);
+        } else {
+          setHasPermission(status === false);
+        }
+      // create asset 
+    
+    } catch(error) {
+      console.log(error)
+    }
+  }
+ 
   if (hasPermission === true) {
     return (
       <CenterView>
@@ -52,6 +115,13 @@ export default function App() {
             marginBottom: 50,
           }}
           type={cameraType}
+          onFacesDetected={smileDetected ? null : onFacesDetected}
+          faceDetectorSettings={{
+            mode: FaceDetector.Constants.Mode.fast,
+            detectLandmarks: FaceDetector.Constants.Landmarks.all,
+            runClassifications: FaceDetector.Constants.Classifications.all
+          }}
+          ref={cameraRef}
         />
         <TouchableOpacity onPress={reverseCamera}>
             <MaterialCommunityIcons
@@ -60,6 +130,7 @@ export default function App() {
               color="white"
             />
         </TouchableOpacity>
+        <Text style={{marginTop:20}}>Smile Gauge {String(smilePercentage*100).slice(0,1)} %</Text>
       </CenterView>
     );
   } else if (hasPermission === false) {

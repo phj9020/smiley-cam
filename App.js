e29396ee129c073237af7afaed50850733d29277
HanJin Park <phj9020@hotmail.com>
2021-03-22 14:14:18 +0900

    Camera permissions function and Camera component added

--- App.js
+++ App.js
@@ -1,21 +1,78 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator, Dimensions, TouchableOpacity } from "react-native";
+import { Camera } from "expo-camera";
+import styled from "styled-components/native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+const { width: WIDTH, height: HEIGHT } = Dimensions.get("window");
+
+const CenterView = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: cornflowerblue;
+`;
+
+const Text = styled.Text`
+  color: white;
+  font-size: 18px;
+`;
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [hasPermission, setHasPermission] = useState(null);
+  const [cameraType, setCameraType] = useState(Camera.Constants.Type.front);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      if (status === "granted") {
+        setHasPermission(status === "granted");
+      } else {
+        setHasPermission(status === false);
+      }
+    })();
+  }, []);
+
+  const reverseCamera = () => {
+    if (cameraType === Camera.Constants.Type.front) {
+      setCameraType(Camera.Constants.Type.back);
+    } else if (cameraType === Camera.Constants.Type.back) {
+      setCameraType(Camera.Constants.Type.front);
+    }
+  };
+  console.log(hasPermission);
+
+  if (hasPermission === true) {
+    return (
+      <CenterView>
+        <Camera
+          style={{
+            width: WIDTH - 40,
+            height: HEIGHT / 1.5,
+            marginBottom: 50,
+          }}
+          type={cameraType}
+        />
+        <TouchableOpacity onPress={reverseCamera}>
+            <MaterialCommunityIcons
+              name={cameraType === Camera.Constants.Type.front ? "camera-rear" : "camera-front"}
+              size={50}
+              color="white"
+            />
+        </TouchableOpacity>
+      </CenterView>
+    );
+  } else if (hasPermission === false) {
+    return (
+      <CenterView>
+        <Text>No access to camera</Text>
+      </CenterView>
+    );
+  } else {
+    return (
+      <CenterView>
+        <ActivityIndicator size="large" color="#00ff00" />
+      </CenterView>
+    );
+  }
+}
